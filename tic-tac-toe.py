e9c89e97b6ddfa886083d331de95962d38a1b099
gkwk12 <33406195+gkwk12@users.noreply.github.com>
2017-11-26 13:26:43 +0900

    601621842 채지희
    코드완성제출입니다.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -27,4 +27,62 @@ for i in range(9) :
 
 window.mainloop()
 
+from tkinter import *
+import tkinter.messagebox
+
+def checked(i) :
+      
+      global player
+      button = list[i]
+
+      if button["text"] != "     " :
+            return
+      button["text"] = player 
+      button["bg"] = "yellow"
+
+      if player == "X" :
+            player = "O"
+            button["bg"] = "yellow"
+            Checked_W()
+      else :
+            player = "X"
+            button["bg"] = "lightgreen"
+            Checked_W()
+            
+def Checked_W():
+       if list[0]["text"] == list[3]["text"] == list[6]["text"] != "     ":
+            win()
+       elif list[1]["text"] == list[4]["text"] == list[7]["text"] != "     ":
+            win()   
+       elif list[2]["text"] == list[5]["text"] == list[8]["text"] != "     ":
+            win()
+       elif list[0]["text"] == list[1]["text"] == list[2]["text"] != "     ":
+            win()
+       elif list[3]["text"] == list[4]["text"] == list[5]["text"] != "     ":
+            win()
+       elif list[6]["text"] == list[7]["text"] == list[8]["text"] != "     ":
+            win()
+       elif list[0]["text"] == list[4]["text"] == list[8]["text"] != "     ":
+            win()
+       elif list[2]["text"] == list[4]["text"] == list[6]["text"] != "     ":
+            win()
+            
+window = Tk()
+player = "X"
+list= []
+
+for i in range(9) :
+      b = Button(window, text="     ", command=lambda k=i: checked(k))
+      b.grid(row=i//3, column=i%3)
+      list.append(b)
+       
+def win():
+      if player == "X":
+            tkinter.messagebox.showinfo("WINNER", "player 0")
+            quit()
+      else:
+            tkinter.messagebox.showinfo("WINNER", "player X")
+            quit()
+
+window.mainloop()
 
